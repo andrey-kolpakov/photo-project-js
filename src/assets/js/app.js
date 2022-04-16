52a9071a3d6e117a0cb3449f2443bd4cab349857
Andrey Kolpakov <gfstels@yandex.ru>
2022-04-16 14:02:56 +0600

    new

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,1 +1,27 @@
 console.log('file 1');
+
+function toggleNewPopupContainer(name){
+    let newPopup = document.querySelector(name)
+    newPopup.classList.toggle(name.slice(1)+'--visible')
+}
+
+let popup = document.querySelector('.popup')
+let toggleEditProfile = document.getElementsByName('toggle-popup-edit-profile')
+let addPhoto = document.getElementsByName('toggle-popup-add-photo')
+
+
+
+
+toggleEditProfile.forEach(element => {
+    element.addEventListener('click', function(){
+        popup.classList.toggle('popup--popup_opened')
+        toggleNewPopupContainer('.popup__container--edit-profile')
+    })
+})
+
+addPhoto.forEach(element => {
+    element.addEventListener('click', function(){
+        popup.classList.toggle('popup--popup_opened')
+        toggleNewPopupContainer('.popup__container--add-photo')
+    })
+})
