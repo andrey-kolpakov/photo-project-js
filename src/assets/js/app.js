3ab2f5ee8cb0c32b5f691e07879fd54315455f0e
Andrey Kolpakov <gfstels@yandex.ru>
2022-04-16 14:52:38 +0600

    gulpfile-edit

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -5,7 +5,7 @@ function toggleNewPopupContainer(name){
     newPopup.classList.toggle(name.slice(1)+'--visible')
 }
 
-let popup = document.querySelector('.popup')
+let popupsContainer = document.querySelector('.popup')
 let toggleEditProfile = document.getElementsByName('toggle-popup-edit-profile')
 let addPhoto = document.getElementsByName('toggle-popup-add-photo')
 
@@ -14,16 +14,16 @@ let addPhoto = document.getElementsByName('toggle-popup-add-photo')
 
 toggleEditProfile.forEach(element => {
     element.addEventListener('click', function(){
-        popup.classList.toggle('popup--popup_opened')
+        popupsContainer.classList.toggle('popup--popup_opened')
         toggleNewPopupContainer('.popup__container--edit-profile')
     })
 })
 
 addPhoto.forEach(element => {
     element.addEventListener('click', function(){
-        popup.classList.toggle('popup--popup_opened')
+        popupsContainer.classList.toggle('popup--popup_opened')
         toggleNewPopupContainer('.popup__container--add-photo')
     })
 }) 
 
-//asdasdasd
+//Назвать popup outer
