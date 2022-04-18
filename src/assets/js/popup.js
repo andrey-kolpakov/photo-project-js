d892fcf4518fd17d2c1a974ab8effcfb2f0c1023
Andrey Kolpakov <gfstels@yandex.ru>
2022-04-18 20:58:40 +0600

    numbersOfCompletedTasksFromFirstChecklist-1-3-4-6-7

---
+++ src/assets/js/popup.js
@@ -0,0 +1,30 @@
+function toggleNewPopupInner(name){
+    let newPopup = document.querySelector(name)
+    newPopup.classList.toggle(name.slice(1)+'--visible')
+}
+
+let popupGroup = document.querySelector('.popup-group')
+let toggleEditProfile = document.getElementsByName('toggle-popup-edit-profile')
+let addPhoto = document.getElementsByName('toggle-popup-add-photo')
+
+toggleEditProfile.forEach(element => {
+    element.addEventListener('click', function(){
+        popupGroup.classList.toggle('popup-group--popup_opened')
+        toggleNewPopupInner('.popup__inner--edit-profile')
+
+        let userName = document.getElementById('user-name')
+        let inputName = document.getElementById('input-name')
+        inputName.value = userName.textContent
+
+        let userDescription = document.getElementById('user-description')
+        let inputDescription = document.getElementById('input-description')
+        inputDescription.value = userDescription.textContent
+    })
+})
+
+addPhoto.forEach(element => {
+    element.addEventListener('click', function(){
+        popupGroup.classList.toggle('popup-group--popup_opened')
+        toggleNewPopupInner('.popup__inner--add-photo')
+    })
+}) 
