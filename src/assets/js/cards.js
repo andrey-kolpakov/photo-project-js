122abe7fe503e2bbdd2b1cd356a4729e342c02ac
Andrey Kolpakov <gfstels@yandex.ru>
2022-04-26 22:32:18 +0600

    deleteButton-addNewCardForm-aLotOfBugs

--- src/assets/js/cards.js
+++ src/assets/js/cards.js
@@ -25,39 +25,62 @@ const initialCards = [
     }
 ];
 
-let firstRow = document.getElementById('firstRow')
+let generalRow = document.getElementById('generalRow')
+console.log(generalRow)
 
-function loadInitialCards() {
-    initialCards.forEach(card => {
-        let newCol = document.createElement('div')
-        newCol.classList.add('col-xlg-4', 'col-md-12')
+function cardRender(card, row, prepend = false) {
+    let newCol = document.createElement('div')
+    newCol.classList.add('col-xlg-4', 'col-md-12')
 
-        let newCard = document.createElement('div')
-        newCard.classList.add('card')
+    let newCard = document.createElement('div')
+    newCard.classList.add('card')
 
-        let newImage = document.createElement('div')
-        newImage.classList.add('card__image')
-        newImage.style.backgroundImage = `url('${card.link}')`;
+    let newImage = document.createElement('div')
+    newImage.classList.add('card__image')
+    newImage.style.backgroundImage = `url('${card.link}')`;
 
-        let newCardTextWrapper = document.createElement('div') 
-        newCardTextWrapper.classList.add('card__text-wrapper')
+    let newCardTextWrapper = document.createElement('div')
+    newCardTextWrapper.classList.add('card__text-wrapper')
 
-        let newText = document.createElement('div')
-        newText.classList.add('card__text') 
-        newText.textContent = card.name
-
-        let cardLikeButton = document.createElement('button')
-        cardLikeButton.classList.add('card__like-button') 
-
-        newCard.appendChild(newImage)
-        newCard.appendChild(newCardTextWrapper)
-        
-        newCardTextWrapper.appendChild(newText)
-        newCardTextWrapper.appendChild(cardLikeButton)
-
-        newCol.appendChild(newCard)
-        firstRow.appendChild(newCol)
+    let deleteButton = document.createElement('div')
+    deleteButton.classList.add('card__delete')
+    deleteButton.addEventListener('click', function(){
+        let thisCard = document.getElementById(card.name)
+        thisCard.remove()
     })
+
+    let newText = document.createElement('div')
+    newText.classList.add('card__text')
+    newText.textContent = card.name
+
+    let cardLikeButton = document.createElement('button')
+    cardLikeButton.classList.add('card__like-button')
+    cardLikeButton.addEventListener('click', function () {
+        cardLikeButton.classList.toggle('card__like-button--active')
+    })
+
+    newCard.appendChild(newImage)
+    newCard.appendChild(deleteButton)
+    newCard.appendChild(newCardTextWrapper)
+    
+
+    newCardTextWrapper.appendChild(newText)
+    newCardTextWrapper.appendChild(cardLikeButton)
+
+    newCol.appendChild(newCard)
+    newCol.id = card.name
+
+    if (prepend) {
+        row.prepend(newCol)
+    } else {
+        row.appendChild(newCol)
+    }
 }
 
-document.addEventListener("DOMContentLoaded", loadInitialCards);
+document.addEventListener("DOMContentLoaded", initialCards.forEach(card => {
+    cardRender(card, generalRow)
+}
+)
+);
+
+//Что-то странное с выводом row в консоль из функции cardRender
--- src/assets/js/forms.js
+++ src/assets/js/forms.js
@@ -1,4 +1,6 @@
-let formEditProfile = document.querySelector('.popup__form-edit-profile')
+// Edit profile form
+
+let formEditProfile = document.getElementById('form-edit-profile')
 
 let userName = document.getElementById('user-name')
 let inputName = document.getElementById('input-name')
@@ -8,16 +10,137 @@ let inputDescription = document.getElementById('input-description')
 
 let addNewPhotoPopup = document.querySelector('.popup-group')
 
-function formSubmitHandler (evt) {
-    evt.preventDefault(); 
+function formSubmitHandler(evt) {
+    evt.preventDefault();
 
     userName.textContent = inputName.value
     userDescription.textContent = inputDescription.value
 
-    console.log(addNewPhotoPopup)
-
-    addNewPhotoPopup.classList.remove('popup-group--popup_opened') 
+    addNewPhotoPopup.classList.toggle('popup-group--popup_opened')
+    document.querySelector('.popup__inner--edit-profile').classList.toggle('popup__inner--edit-profile--visible')
 }
 formEditProfile.addEventListener('submit', formSubmitHandler)
+
+
+
+// New place form
+
+let generalRow = document.getElementById('generalRow')
+
+/* function cardRender(cardName, cardLink, row) {
+    let newCol = document.createElement('div')
+    newCol.classList.add('col-xlg-4', 'col-md-12')
+
+    let newCard = document.createElement('div')
+    newCard.classList.add('card')
+
+    let newImage = document.createElement('div')
+    newImage.classList.add('card__image')
+    newImage.style.backgroundImage = `url('${cardLink}')`;
+
+    let newCardTextWrapper = document.createElement('div')
+    newCardTextWrapper.classList.add('card__text-wrapper')
+
+    let newText = document.createElement('div')
+    newText.classList.add('card__text')
+    newText.textContent = cardName
+
+    let cardLikeButton = document.createElement('button')
+    cardLikeButton.classList.add('card__like-button')
+    cardLikeButton.addEventListener('click', function () {
+        cardLikeButton.classList.toggle('card__like-button--active')
+    })
+
+    newCard.appendChild(newImage)
+    newCard.appendChild(newCardTextWrapper)
+
+    newCardTextWrapper.appendChild(newText)
+    newCardTextWrapper.appendChild(cardLikeButton)
+
+    newCol.appendChild(newCard)
+    row.prepend(newCol)
+} */
+
+function cardRender(card, row) {
+    let newCol = document.createElement('div')
+    newCol.classList.add('col-xlg-4', 'col-md-12')
+
+    let newCard = document.createElement('div')
+    newCard.classList.add('card')
+
+    let newImage = document.createElement('div')
+    newImage.classList.add('card__image')
+    newImage.style.backgroundImage = `url('${card.link}')`;
+
+    let newCardTextWrapper = document.createElement('div')
+    newCardTextWrapper.classList.add('card__text-wrapper')
+
+    let newText = document.createElement('div')
+    newText.classList.add('card__text')
+    newText.textContent = card.name
+
+    let cardLikeButton = document.createElement('button')
+    cardLikeButton.classList.add('card__like-button')
+    cardLikeButton.addEventListener('click', function () {
+        cardLikeButton.classList.toggle('card__like-button--active')
+    })
+
+    newCard.appendChild(newImage)
+    newCard.appendChild(newCardTextWrapper)
+
+    newCardTextWrapper.appendChild(newText)
+    newCardTextWrapper.appendChild(cardLikeButton)
+
+    newCol.appendChild(newCard)
+    row.prepend(newCol)
+}
+
+let formAddNewPlace = document.getElementById('form-add-new-place')
+
+let newPlaceNameInput = document.getElementById('input-name-of-new-place')
+let newPlaceLinkInput = document.getElementById('input-link-on-new-place')
+
+let NewCard = {
+    constructor: function(name, link){
+        this.name = name
+        this.link = link
+
+        return this
+    }
+}
+
+function formAddNewPlaceHandler(evt) {
+    evt.preventDefault();
+
+    if (newPlaceNameInput.value && newPlaceLinkInput.value) {
+        
+        let newCard = Object.create(NewCard).constructor(newPlaceNameInput.value, newPlaceLinkInput.value)
+        console.log(newCard)
+
+        cardRender(newCard, generalRow)
+
+        addNewPhotoPopup.classList.remove('popup-group--popup_opened')
+        document.querySelector('.popup__inner--edit-profile').classList.toggle('popup__inner--add-photo--visible')
+    } else {
+        if(!newPlaceNameInput.value){
+            validateIsLose('input-name-of-new-place-container')
+        }
+        if(!newPlaceLinkInput.value){
+            validateIsLose('input-link-on-new-place-container')
+        }
+    }
+}
+
+function validateIsLose(id){
+    let parrent = document.getElementById(id)
+
+    let errorCaption = parrent.querySelector('.popup__input-error')
+    let errorBorder = parrent.querySelector('.popup__input-border')
+
+    errorCaption.classList.add('popup__input-error--error')
+    errorBorder.classList.add('popup__input-border--error')
+}
+
+formAddNewPlace.addEventListener('submit', formAddNewPlaceHandler)
 
 
